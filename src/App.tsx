ea38385161804cad896bdd254fdd36c50f957fb3
road2paradise <ken_nggg@outlook.com>
2021-07-14 02:36:23 +1200

    Cleaned up App.tsx and App.css, removed unused Props and cleaned up states

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import Web3 from 'web3';
 import { Login } from './pages/Login';
 import { Address } from './pages/Address';
 
-export interface IAccount {
-  account: string;
-}
 
 export interface IWeb3 {
   web3: Web3;
@@ -15,8 +12,7 @@ export interface IWeb3 {
 function App() {
   // In future you wouldnt want to save account as a state, since the user 
   // might already have integrated their metamask onto the browser.
-  const [account, setAccount] = useState<IAccount>({ account: "" });
-  const [web3, setWeb3] = useState<IWeb3>({ web3: new Web3(null) });
+  const [account, setAccount] = useState<string>("");
 
   function connectWallet() {
     checkAccount();
@@ -26,16 +22,15 @@ function App() {
     // Lets assume that we dont have a local blockchain, otherwise
     // use an OR statement for a local blockchain like Web3.givenProvider || http://localhost:somePort.
     const web3 = new Web3(Web3.givenProvider);
-    setWeb3({ web3: web3 });
     const accounts = await web3.eth.getAccounts();
-    setAccount({ account: accounts[0] });
+    setAccount(accounts[0]);
   }
 
 
   return (
     <div className="App">
-      {/* Poor mans react-router here created with if statements.*/}
-      {account.account === "" ? <Login connectWallet={connectWallet} /> : <Address />}
+      {/* Poor mans react-router here created with if statements, otherwise it would be best to implement routes with react-router.*/}
+      {account === "" ? <Login connectWallet={connectWallet} /> : <Address />}
     </div>
   );
 }
